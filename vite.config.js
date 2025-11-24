0d288abf0b7043acde9e26fc3a9ddae308f19676
Vishnubalu.D <vishnudhas4@gmail.com>
2025-11-24 21:45:39 +0530

    build

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [react()],
+    build: {
+        outDir: 'dist',
+    },
+    server: {
+        port: 3000,
+        open: true,
+    },
+    esbuild: {
+        loader: 'jsx',
+        include: /src\/.*\.js$/,
+        exclude: [],
+    },
+    optimizeDeps: {
+        esbuildOptions: {
+            loader: {
+                '.js': 'jsx',
+            },
+        },
+    },
+});
